4dfaf2b20452a6a6dce9edcd032e5463faf4408c
s011208 <s011208@gmail.com>
2017-08-11 13:49:09 +0800

    first commit

---
+++ app/src/main/java/s011208/memorylogger/MainActivity.java
@@ -0,0 +1,14 @@
+package s011208.memorylogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s011208.memoryloggerlib.Test t;
+    }
+}
---
+++ memoryloggerlib/src/main/java/s011208/memoryloggerlib/Test.java
@@ -0,0 +1,8 @@
+package s011208.memoryloggerlib;
+
+/**
+ * Created by s011208 on 2017/8/11.
+ */
+
+public class Test {
+}
